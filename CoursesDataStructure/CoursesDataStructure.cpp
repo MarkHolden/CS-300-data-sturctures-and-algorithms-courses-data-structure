7925043d808dbdd104774930a4dcc541082859de
Mark Holden <mark.holden@monscierge.com>
2022-04-17 16:56:38 -0500

    Add menu

--- CoursesDataStructure/CoursesDataStructure.cpp
+++ CoursesDataStructure/CoursesDataStructure.cpp
@@ -8,10 +8,27 @@
 
 #include "HashTable.h"
 #include "FileParser.h"
+#include <iostream>
+#include "Input.h"
 
 using namespace std;
 
+/// <summary>
+/// Display the main menu.
+/// </summary>
+/// <param name="isFinalRound"></param>
+void DisplayMenu() {
+    cout << "Game Menu:" << endl;
+    cout << "  1. Load Data Structure" << endl;
+    cout << "  2. Print Course List" << endl;
+    cout << "  3. Print Course" << endl;
+    cout << "  0. Exit" << endl;
+}
 
+void PrintCourseInfo(HashTable* const hashTable) {
+    string courseId = Input::ReadString("For which courseId would you like to display info? ");
+    hashTable->PrintCourseInfo(courseId);
+}
 
 /// <summary>
 /// Main entry point for the application.
@@ -19,9 +36,29 @@ using namespace std;
 /// <returns>Exit code.</returns>
 int main() {
     auto hashTable = new HashTable;
-    FileParser::ReadFile(hashTable);
-    hashTable->PrintCourses();
+    int choice = -1;
+    while (choice != 0) {
+        DisplayMenu();
+        choice = Input::ReadInt("Enter Choice: ", 0, 3);
 
-	return 0;
+        switch (choice) {
+        case 1:
+            FileParser::ReadFile(hashTable);
+            break;
+
+        case 2:
+            hashTable->PrintCourses();
+            break;
+
+        case 3:
+            PrintCourseInfo(hashTable);
+            break;
+
+        default:
+            break;
+        }
+    }
+
+    return 0;
 }
 
